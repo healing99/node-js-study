ec420d4158ba80790b86d2f1ee48968a411b8195
yoon <nicealice99@gmail.com>
2021-09-04 02:44:25 +0900

    중복되는 부분 함수로 분리

--- main.js
+++ main.js
@@ -2,6 +2,33 @@ let http = require("http");
 let fs = require("fs");
 let url = require("url");
 
+function templateHTML(title, list, body) {
+  return `
+  <!DOCTYPE html>
+        <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8" />
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${body}
+          </body>
+        </html>
+  `;
+}
+
+function templateList(fileList) {
+  let list = "<ul>";
+  let i = 0;
+  while (i < fileList.length) {
+    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + "</ul>";
+  return list;
+}
 //웹 서버 객체를 만들때 createServer 를 이용
 let app = http.createServer((request, response) => {
   let _url = request.url;
@@ -15,70 +42,19 @@ let app = http.createServer((request, response) => {
         let title = "Welcome";
         data = "Hello, Node.js";
 
-        let list = "<ul>";
-        let i = 0;
-        while (i < fileList.length) {
-          list =
-            list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + "</ul>";
+        let list = templateList(fileList);
 
-        let template = `
-          <!DOCTYPE html>
-        <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8" />
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>
-            ${data}
-            </p>
-          </body>
-        </html>
-          `;
+        let template = templateHTML(title, list, `<h2>${title}</h2>${data}`);
 
         response.writeHead(200);
         response.end(template);
       });
     } else {
       fs.readdir("./data", (error, fileList) => {
-        console.log(fileList);
-        let title = "Welcome";
-        data = "Hello, Node.js";
-
-        let list = "<ul>";
-        let i = 0;
-        while (i < fileList.length) {
-          list =
-            list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + "</ul>";
-
         fs.readFile(`data/${queryData.id}`, "utf8", (err, data) => {
           let title = queryData.id;
-          let template = `
-        <!DOCTYPE html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8" />
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>
-          ${data}
-          </p>
-        </body>
-      </html>
-        `;
+          let list = templateList(fileList);
+          let template = templateHTML(title, list, `<h2>${title}</h2>${data}`);
 
           response.writeHead(200);
           response.end(template);
