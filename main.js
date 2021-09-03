97d47a4658add747a3442ac8172e4edcd65b1dce
yoon <nicealice99@gmail.com>
2021-09-03 22:56:08 +0900

    글 목록 출력하기

--- main.js
+++ main.js
@@ -6,64 +6,83 @@ let url = require("url");
 let app = http.createServer((request, response) => {
   let _url = request.url;
   let queryData = url.parse(_url, true).query; // parse : _url에 대한 정보가 객체로 담긴다
-  let title = queryData.id;
   let pathname = url.parse(_url, true).pathname;
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      let title = "Welcome";
-      data = "Hello, Node.js";
-      let template = `
-        <!DOCTYPE html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8" />
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>
-          ${data}
-          </p>
-        </body>
-      </html>
-        `;
+      fs.readdir("./data", (error, fileList) => {
+        console.log(fileList);
+        let title = "Welcome";
+        data = "Hello, Node.js";
 
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      fs.readFile(`data/${queryData.id}`, "utf8", (err, data) => {
-        let title = queryData.id;
+        let list = "<ul>";
+        let i = 0;
+        while (i < fileList.length) {
+          list =
+            list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+
         let template = `
-        <!DOCTYPE html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8" />
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>
-          ${data}
-          </p>
-        </body>
-      </html>
-        `;
+          <!DOCTYPE html>
+        <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8" />
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>
+            ${data}
+            </p>
+          </body>
+        </html>
+          `;
 
         response.writeHead(200);
         response.end(template);
+      });
+    } else {
+      fs.readdir("./data", (error, fileList) => {
+        console.log(fileList);
+        let title = "Welcome";
+        data = "Hello, Node.js";
+
+        let list = "<ul>";
+        let i = 0;
+        while (i < fileList.length) {
+          list =
+            list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+
+        fs.readFile(`data/${queryData.id}`, "utf8", (err, data) => {
+          let title = queryData.id;
+          let template = `
+        <!DOCTYPE html>
+      <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8" />
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>
+          ${data}
+          </p>
+        </body>
+      </html>
+        `;
+
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
