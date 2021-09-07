dd707a6c03fd1f43a3b15ae517c22fe3852877ff
yoon <nicealice99@gmail.com>
2021-09-08 02:23:56 +0900

    모듈 이용해서 전반적인 구조 정리정돈

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const http = require("http");
 const url = require("url");
-const qs = require("querystring");
-const template = require("./lib/template.js");
-const db = require("./lib/db");
+const topic = require("./lib/topic");
 
 //웹 서버 객체를 만들때 createServer 를 이용
 //createServer로 전달된 콜백함수는 두개의 인자 (request, response)
@@ -16,164 +14,20 @@ let app = http.createServer((request, response) => {
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      db.query(`SELECT * FROM topic`, (error, topics) => {
-        const title = "Welcome";
-        description = "Hello, Node.js";
-        const list = template.list(topics);
-        const html = template.HTML(
-          title,
-          list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
+      topic.home(request, response);
     } else {
-      db.query("SELECT * FROM topic", (error, topics) => {
-        if (error) {
-          throw error;
-        }
-        //해당하는 id의 정보 가져오기
-        db.query(
-          `SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,
-          [queryData.id],
-          (error2, topic) => {
-            if (error2) {
-              throw error2;
-            }
-            const title = topic[0].title;
-            description = topic[0].description;
-            const list = template.list(topics);
-            const html = template.HTML(
-              title,
-              list,
-              `<h2>${title}</h2>${description}
-              <p>by ${topic[0].name}</p>
-              `,
-              `<a href="/create">create</a>
-             <a href="/update?id=${queryData.id}">update</a>
-             <form action="delete_process" method="post">
-             <input type="hidden" name="id" value="${queryData.id}">
-             <input type="submit" value="delete">
-             </form>
-              `
-            );
-            response.writeHead(200);
-            response.end(html);
-          }
-        );
-      });
+      topic.page(request, response);
     }
   } else if (pathname === "/create") {
-    db.query(`SELECT * FROM topic`, (error, topics) => {
-      db.query("SELECT * FROM author", (error2, authors) => {
-        const title = "Create";
-        const list = template.list(topics);
-        const html = template.HTML(
-          title,
-          list,
-          `
-      <form action="/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"/></p>
-      <p><textarea name="description" placeholder="description"></textarea></p>
-      <p>
-      ${template.authorSelect(authors)}
-      </p>
-      <p><input type="submit" /></p>
-      </form>
-      `,
-          ""
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
-    });
+    topic.create(request, response);
   } else if (pathname === "/create_process") {
-    let body = "";
-    request.on("data", (data) => {
-      //request에 data가 있을 경우 처리하는 부분
-      body = body + data;
-    });
-    request.on("end", () => {
-      //request에 data 처리가 다 끝났음을 알려주는 부분
-      const post = qs.parse(body); //parse : 객체화
-
-      db.query(
-        `INSERT INTO topic (title, description, created, author_id) VALUES(?, ?,  NOW(), ?);`,
-        [post.title, post.description, post.author],
-        (error, result) => {
-          if (error) throw error;
-          response.writeHead(302, { Location: `/?id=${result.insertId}` }); //getting the id of an inserted row 방법
-          response.end();
-        }
-      );
-    });
+    topic.create_process(request, response);
   } else if (pathname === "/update") {
-    db.query("SELECT * FROM topic", (error, topics) => {
-      if (error) throw error;
-      db.query(
-        "SELECT * FROM topic WHERE id=?",
-        [queryData.id],
-        (error2, topic) => {
-          if (error2) throw error2;
-          db.query("SELECT * FROM author", (error3, authors) => {
-            const list = template.list(topics);
-            const html = template.HTML(
-              topic[0].title,
-              list,
-              `
-          <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${topic[0].id}">
-          <p><input type="text" name="title" placeholder="title" value="${
-            topic[0].title
-          }"/></p>
-          <p><textarea name="description" placeholder="description">${
-            topic[0].description
-          }</textarea></p>
-          <p>${template.authorSelect(authors, topic[0].author_id)}</p>
-          <p><input type="submit" /></p>
-          </form>
-          `,
-              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-            );
-
-            response.writeHead(200);
-            response.end(html);
-          });
-        }
-      );
-    });
+    topic.update(request, response);
   } else if (pathname === "/update_process") {
-    let body = "";
-    request.on("data", (data) => {
-      body = body + data;
-    });
-    request.on("end", () => {
-      const post = qs.parse(body);
-      db.query(
-        "UPDATE topic SET title=?, description=?, author_id=? WHERE id=?",
-        [post.title, post.description, post.author, post.id],
-        (error, result) => {
-          if (error) throw error;
-          response.writeHead(302, { Location: `/?id=${post.id}` }); //status code 302 : 주어진 URL에 일시적으로 이동되었음을 가리킴
-          response.end();
-        }
-      );
-    });
+    topic.update_process(request, response);
   } else if (pathname === "/delete_process") {
-    let body = "";
-    request.on("data", (data) => {
-      body = body + data;
-    });
-    request.on("end", () => {
-      const post = qs.parse(body);
-      db.query("DELETE FROM topic WHERE id=?", [post.id], (error, result) => {
-        if (error) throw error;
-        response.writeHead(302, { Location: "/" });
-        response.end();
-      });
-    });
+    topic.delete_process(request, response);
   } else {
     response.writeHead(404);
     response.end("Not Found");
