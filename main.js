91094d159b0184c8c92478a3ba56a9637bf8d2d8
yoon <nicealice99@gmail.com>
2021-09-22 15:19:29 +0900

    라우팅 및 res.send() 관련

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+//라우팅
+app.get("/", (req, res) => res.send("Main"));
+
+app.get("/about", (req, res) => res.send("About"));
 
 app.listen(3000, () => console.log("Example app listening on port 3000"));
+
+/*
+라우팅
+app.METHOD(PATH, HANDLER)
+- app은 express의 인스턴스
+- METHOD는 HTTP 요청 메소드 (get, post, put, delete)
+- PATH는 서버에서의 경로
+- HANDLER는 라우터가 일치할때 실행되는 함수
+
+------------------------------------
+
+res.send([body])
+클라이언트에 응답 데이터를 보낸다. 전달할 수 있는 데이터에는
+HTML 문자열, Buffer 객체, JSON 객체, JSON 배열 등이 있다
+*/
