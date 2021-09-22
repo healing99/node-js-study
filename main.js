70e513aa9e3a1d8133ebb5142a7059e0a78a16f6
yoon <nicealice99@gmail.com>
2021-09-23 01:12:04 +0900

    라우터 - 파일로 분리

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const template = require("./lib/template");
-const path = require("path");
-const sanitizeHtml = require("sanitize-html");
-const qs = require("querystring");
 const bodyParser = require("body-parser");
 const compression = require("compression"); //압축된 방식으로 데이터 사용할 수 있음
+const topicRouter = require("./routes/topic");
 
 app.use(express.static("public")); //public 디렉토리안에서 정적파일을 찾겠다
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,115 +33,7 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
-app.get("/topic/create", (req, res) => {
-  const title = "WEB - create";
-  const list = template.list(req.list);
-  const html = template.HTML(
-    title,
-    list,
-    `
-      <form action="/topic/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `,
-    ""
-  );
-  res.send(html);
-});
-
-app.post("/topic/create_process", (req, res) => {
-  const post = req.body;
-  const title = post.title;
-  const description = post.description;
-  fs.writeFile(`data/${title}`, description, "utf8", (err) => {
-    res.redirect(`/topic/${title}`);
-    // res.writeHead(302, { Location: `/?id=${title}` });
-    // res.end();
-  });
-});
-
-app.get("/topic/update/:pageId", (req, res) => {
-  const filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", (err, description) => {
-    const title = req.params.pageId;
-    const list = template.list(req.list);
-    const html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/topic/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-    );
-    res.send(html);
-  });
-});
-
-app.post("/topic/update_process", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const title = post.title;
-  const description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, (error) => {
-    fs.writeFile(`data/${title}`, description, "utf8", (error) => {
-      res.redirect(`/topic/${title}`);
-      // res.writeHead(302, { Location: `/?id=${title}` });
-      // res.end();
-    });
-  });
-});
-
-app.post("/topic/delete_process", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, (error) => {
-    res.redirect("/");
-  });
-});
-
-app.get("/topic/:pageId", (req, res, next) => {
-  const filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", (error, description) => {
-    if (error) {
-      next(error); //아래에 작성한 4개의 인자를 가진 미들웨어 호출됨
-    } else {
-      const title = req.params.pageId;
-      const sanitizedTitle = sanitizeHtml(title);
-      const sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"]
-      });
-      const list = template.list(req.list);
-      const html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-            <a href="/topic/update/${sanitizedTitle}">update</a>
-            <form action="/topic/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-      );
-      res.send(html);
-    }
-  });
-});
+app.use("/topic", topicRouter); // "/topic"으로 시작하는 주소에 topicRouter라는 이름의 미들웨어를 적용하겠다
 
 //더 이상 실행할 미들웨어x 경우
 app.use((req, res, next) => {
---
+++ routes/topic.js
@@ -0,0 +1,132 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const fs = require("fs");
+const sanitizeHtml = require("sanitize-html");
+const template = require("../lib/template");
+const qs = require("querystring");
+
+router.get("/create", (req, res) => {
+  const title = "WEB - create";
+  const list = template.list(req.list);
+  const html = template.HTML(
+    title,
+    list,
+    `
+      <form action="/topic/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `,
+    ""
+  );
+  res.send(html);
+});
+
+router.post("/create_process", (req, res) => {
+  const post = req.body;
+  const title = post.title;
+  const description = post.description;
+  fs.writeFile(`data/${title}`, description, "utf8", (err) => {
+    res.redirect(`/topic/${title}`);
+    // res.writeHead(302, { Location: `/?id=${title}` });
+    // res.end();
+  });
+});
+
+router.get("/update/:pageId", (req, res) => {
+  const filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, "utf8", (err, description) => {
+    const title = req.params.pageId;
+    const list = template.list(req.list);
+    const html = template.HTML(
+      title,
+      list,
+      `
+        <form action="/topic/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+    );
+    res.send(html);
+  });
+});
+
+router.post("/update_process", (req, res) => {
+  const post = req.body;
+  const id = post.id;
+  const title = post.title;
+  const description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, (error) => {
+    fs.writeFile(`data/${title}`, description, "utf8", (error) => {
+      res.redirect(`/topic/${title}`);
+      // res.writeHead(302, { Location: `/?id=${title}` });
+      // res.end();
+    });
+  });
+});
+
+router.post("/delete_process", (req, res) => {
+  const post = req.body;
+  const id = post.id;
+  const filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, (error) => {
+    res.redirect("/");
+  });
+});
+
+router.get("/:pageId", (req, res, next) => {
+  const filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, "utf8", (error, description) => {
+    if (error) {
+      next(error); //아래에 작성한 4개의 인자를 가진 미들웨어 호출됨
+    } else {
+      const title = req.params.pageId;
+      const sanitizedTitle = sanitizeHtml(title);
+      const sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"]
+      });
+      const list = template.list(req.list);
+      const html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/topic/create">create</a>
+            <a href="/topic/update/${sanitizedTitle}">update</a>
+            <form action="/topic/delete_process" method="post">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
+              <input type="submit" value="delete">
+            </form>`
+      );
+      res.send(html);
+    }
+  });
+});
+
+module.exports = router;
+
+/*
+주의
+main.js에서
+app.use("/topic", topicRouter); "/topic"으로 시작하는 주소에 topicRouter라는 이름의 미들웨어를 적용하겠다
+
+위와 같이 "/topic"로 라우터를 지정하면,
+내부에서는 "/topic"을 path에 담을 필요가 없다!
+router.get("/topic/create", (req, res) => {});
+이부분을 router.get("/create", (req, res) => {}); 로 변경
+
+--------------------------------------------------------
+ */
