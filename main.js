8a04eb298bf55843f4fd657ce4af12a73f77e7e9
yoon <nicealice99@gmail.com>
2021-09-22 17:18:17 +0900

    홈에 글 목록 출력

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const template = require("./lib/template.js");
 
 //라우팅
-app.get("/", (req, res) => res.send("Main"));
-
-app.get("/about", (req, res) => res.send("About"));
+app.get("/", (req, res) => {
+  fs.readdir("./data", (error, fileList) => {
+    const title = "Welcome";
+    const description = "Hello, Node.js";
+    const list = template.list(fileList);
+    const html = template.HTML(
+      title,
+      list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    res.send(html);
+  });
+});
 
 app.listen(3000, () => console.log("Example app listening on port 3000"));
 
