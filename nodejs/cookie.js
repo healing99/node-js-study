c035b6b959df4b6ad0cb457b6ce9d67310d7d472
yoon <nicealice99@gmail.com>
2021-10-01 14:17:23 +0900

    쿠키의 생성과 읽기

---
+++ nodejs/cookie.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const cookie = require("cookie");
+
+http
+  .createServer((request, response) => {
+    let cookies = {};
+    //쿠키가 존재할때(쿠키 읽기)
+    if (request.headers.cookie !== undefined) {
+      cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
+
+    //쿠키 생성
+    response.writeHead(200, {
+      "Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"]
+    });
+    response.end("Cookie");
+  })
+  .listen(3000);
